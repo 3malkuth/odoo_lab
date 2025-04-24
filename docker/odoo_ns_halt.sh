fd79a3c74a7fb60cf827fa296b82b6ea2dbe2c35
3malkuth <syst3m5@proton.me>
2025-04-24 14:45:10 +0200

    updated docker scripts

---
+++ docker/odoo_ns_halt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# In order to avoid using sudo for docker you need to run the following commands
+# sudo usermod -aG docker your-desktop-username
+# newgrp docker # activates change without having to login and out
+
+docker-compose -f docker-compose-odoo-no-save.yml down --remove-orphans
+
---
+++ docker/pg_ns_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker-compose -f docker-compose-pg-no-save.yml up -d
+
