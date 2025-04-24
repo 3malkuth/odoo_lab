bb73ab534cc7113612ea2978d2e1af435195eef8
3malkuth <syst3m5@proton.me>
2025-04-24 13:54:48 +0200

    updated naming conventions of the docker files and scripts

--- docker/odoo_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# In order to avoid using sudo for docker you need to run the following commands
-# sudo usermod -aG docker your-desktop-username
-# newgrp docker # activates change without having to login and out
-
-docker-compose up -d
