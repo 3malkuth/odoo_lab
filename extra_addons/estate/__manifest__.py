175d7e583dc1f5480913174b9a5d21452ceb5464
3malkuth <syst3m5@proton.me>
2025-04-25 17:47:30 +0200

    added the start of an example app for odoo

---
+++ extra_addons/estate/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+    'summary': """
+        Example real estate app."
+    """,
+
+    'description': """
+        Example real estate app."
+    """,
+
+    'version': '0.1',
+    'application': True,
+    'category': 'Industries',
+    'installable': True,
+    'depends': ['base'],
+    'license': 'AGPL-3'
+}
